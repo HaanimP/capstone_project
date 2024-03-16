9b5ffe73a7551ecbfa8133f4dfd3d4f18573a983
Tohiera Pietersen <haanimpietersen@gmail.com>
2024-03-16 07:17:11 +0200

    fixed footer

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,8 +1,38 @@
 <template>
-    <footer>
-        <p>All rights reserved to Haanim's Closet <i class="far fa-copyright"></i> {{ currentYear }}.</p>
-    </footer>
+  <footer class="footer">
+    <div class="container py-5">
+      <div class="row gx-5">
+        <!-- Image Column -->
+        <div class="col-lg-2 col-md-6">
+          <img src="https://i.ibb.co/cN7x6bJ/background-7.jpg" alt="Footer Image" class="img-fluid" style="max-height: 150px;">
+        </div>
+        <!-- Content Columns -->
+        <div class="col-lg-4 col-md-6">
+          <h5 class="footer-title">Haanim's Closet</h5>
+          <p class="footer-text">When virtue and modesty enlighten her charms, the lustre of a beautiful woman is brighter than the stars of heaven, and the influence of her power it is in vain to resist.</p>
+          <p class="footer-text mb-0">All rights reserved to Haanim's Closet <i class="far fa-copyright"></i> {{ currentYear }}.</p>
+        </div>
+        <div class="col-lg-2 col-md-6">
+          <h5 class="footer-title">Quick links</h5>
+          <ul class="list-unstyled footer-list">
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/about">About</router-link></li>
+            <li><router-link to="/signup">Sign Up</router-link></li>
+          </ul>
+        </div>
+        <div class="col-lg-2 col-md-6">
+          <h5 class="footer-title">Sister Companies</h5>
+          <ul class="list-unstyled footer-list">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Get started</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
+
 <script>
 export default {
   name: 'FooterComponent',
@@ -13,11 +43,36 @@ export default {
   },
 };
 </script>
+
 <style>
-footer {
-  background-color: #fff;
-  padding: 10px;
-  color: #000;
-  text-align: center;
+.footer {
+  background-color: #f8f9fa;
+  padding-top: 50px;
 }
+
+.footer-title {
+  color: #333;
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+
+.footer-text {
+  color: #666;
+  font-size: 1.25rem;
+}
+
+.footer-list {
+  color: #666;
+}
+
+.footer-list a {
+  color: #666;
+  text-decoration: none;
+  transition: color 0.3s ease;
+}
+
+.footer-list a:hover {
+  color: #333;
+}
+
 </style>
