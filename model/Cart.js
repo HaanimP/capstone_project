7cf911fd70bc8e52a9fc323a11be6a331158fab6
Tohiera Pietersen <haanimpietersen@gmail.com>
2024-03-21 13:44:39 +0200

    cart

--- model/Cart.js
+++ model/Cart.js
@@ -1,60 +1,55 @@
 import {connection as db} from "../config/index.js"
-class Cart{
-       fetchCart(req,res){
-       const qry = `
-       select cartID userID prodID quantity
-       from cart;`
-        db.query(qry, (err, results)=>{
-           if(err) throw err
-           res.json({
-               status : res.statusCode,
-               results
-           })
-        })
-   }
-   fetchCart(req,res){
-       const qry = `
-       select cartID userID prodID quantity
-       from cart WHERE prodID=${req.params.id};`
-        db.query(qry, (err, result)=>{
-           if(err) throw err
-           res.json({
-               status : res.statusCode,
-               result : result[0]
-           })
-        })
-   }
-   deleteCart(req,res){
-     const qry=`DELETE FROM cart ;`
+class Cart {
+  fetchCart(req, res) {
+    const qry = `
+      SELECT cartID, userID, prodID, quantity
+      FROM cart;`;
+    db.query(qry, (err, results) => {
+      if (err) throw err;
+      res.json({
+        status: res.statusCode,
+        results
+      });
+    });
+  }
 
-     db.query(qry, (err)=>{
-       if(err) throw err
-       
-       res.json({
-         status: res.statusCode,
-         msg:'Product was deleted from cart!'
-       })
+  fetchCartById(req, res) {
+    const prodID = db.escape(req.params.id);
+    const qry = `
+      SELECT cartID, userID, prodID, quantity
+      FROM cart
+      WHERE prodID=${prodID};`;
+    db.query(qry, (err, result) => {
+      if (err) throw err;
+      res.json({
+        status: res.statusCode,
+        result: result[0]
+      });
+    });
+  }
 
-     })
- }
-deleteCart(req, res) {
- const qry = `DELETE FROM products WHERE prodID=${db.escape(req.params.id)};`;
+  deleteCart(req, res) {
+    const qry = `DELETE FROM cart;`;
+    db.query(qry, (err) => {
+      if (err) throw err;
+      res.json({
+        status: res.statusCode,
+        msg: 'All products were deleted from cart!'
+      });
+    });
+  }
 
- db.query(qry, (err) => {
-   if (err) {
-     throw err;
-   } else {
-     res.json({
-       status: res.statusCode,
-       msg: 'Product is deleted from cart!',
-     });
-   }
- });
+  deleteCartItem(req, res) {
+    const prodID = db.escape(req.params.id);
+    const qry = `DELETE FROM cart WHERE prodID=${prodID};`;
+    db.query(qry, (err) => {
+      if (err) throw err;
+      res.json({
+        status: res.statusCode,
+        msg: 'Product was deleted from cart!'
+      });
+    });
+  }
 }
 
-
-
-}
-export{
-   Cart
-}
+export { Cart };
